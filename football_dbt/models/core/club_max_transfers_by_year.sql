8a4dec9fd200904748b1ede200cb733ef18e0a9f
mateo2181 <mateomerlo@gmail.com>
2022-03-16 00:31:05 +0100

    Added model: club_max_transfers_by_year

---
+++ football_dbt/models/core/club_max_transfers_by_year.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='table') }}
+
+with transfersClub as (
+    SELECT year, club_name, sum(fee_cleaned) as total
+    FROM {{ source('staging','transfers') }}
+    WHERE transfer_movement='in'
+    group by year, club_name
+    order by total desc
+),
+groupsByYear as (
+    select *
+    from (
+        select tc.*, row_number() over (partition by year order by total desc) rn
+        from transfersClub tc
+    ) tc
+    where rn=1
+    order by year
+)
+select 
+    year as year,
+    club_name as club,
+    CAST(total as decimal) as total,
+from groupsByYear;
